0fa65738732d5de344f687dc7a6701cc6e55c4f8
DarrenFitz <dfitzpatrick12@gmail.com>
2016-11-24 00:45:55 +0000

    Create user table in SQLite. Login/Register functions

---
+++ AppDevProject/Common/Database.cs
@@ -0,0 +1,22 @@
+﻿using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDevProject.Common
+{
+    string path;
+    SQLiteConnection conn;
+
+    public Database()
+    {
+        path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+            "MyDatabase.sqlite");
+        conn = new SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        //Create Table
+        conn.CreateTable<User>();
+    }
+}
